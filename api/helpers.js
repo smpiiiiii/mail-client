685aafe5c1d4109a88f5b2486a6d6393665026c5
smpiiiiii <shimpei.yokoyama0205@gmail.com>
2026-04-11 02:20:52 +0900

    fix: use gmail service preset for SMTP

--- api/helpers.js
+++ api/helpers.js
@@ -149,9 +149,7 @@ let transporter;
 function getTransporter() {
   if (!transporter) {
     transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      service: 'gmail',
       auth: {
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASSWORD
